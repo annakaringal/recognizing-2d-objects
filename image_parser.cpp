a8d532219ace0c6b08550b151cd4464673b4dc93
Anna Karingal <anna.karingal@gmail.com>
2015-09-12 16:46:50 -0400

    Include libraries and remove error checking comment

--- image_parser.cpp
+++ image_parser.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+
+#include "pgm.h"
 
 using namespace std;
 
 bool is_valid_number(const char * str){
-  cout << "checking " << str << endl;
-
   for (int i=0; i < strlen(str); i++){
     if (!isdigit(str[i]) && str[i] != '.') {
       return false;
