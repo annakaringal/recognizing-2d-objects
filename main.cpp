c1cf31511009dc6cf009db245efdb3b7c3df70af
Anna Karingal <anna.karingal@gmail.com>
2015-09-12 14:46:28 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  recognizing-2d-objects
+//
+//  Created by Anna Karingal on 12/9/15.
+//  Copyright (c) 2015 Anna Karingal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
