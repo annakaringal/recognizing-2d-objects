33679d63df067488a020bf6a581cd25bc1fabe23
Anna Karingal <anna.karingal@gmail.com>
2015-09-12 15:33:57 -0400

    Check if threshold input is a valid number

---
+++ functions.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_valid_number(const char * str);
--- p1.cpp
+++ p1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 
+#include "functions.h"
+
 using namespace std;
 
 int main(int argc, const char * argv[]) {
@@ -9,14 +11,26 @@ int main(int argc, const char * argv[]) {
       cerr << "ERROR: Invalid number of arguments." << endl;
       exit(-1);
     }
-    // TODO: Check var types
+
     string input_img_name = argv[1];
-    string threshold = argv[2];
     string output_img_name = argv[3];
+    int threshold;
+
+    if (is_valid_number(argv[2])){
+      threshold = atoi(argv[2]);
+    } else {
+      cerr << "ERROR: Invalid threshold number." << endl;
+      exit(-1);
+    }
 
     // Open image file
     ifstream read_img; 
     read_img.open(input_img_name.c_str());
+
+    cout << "threshold: " << threshold << endl;
+    cout << "inptu: " << input_img_name << endl;
+    cout << "ouput: " << output_img_name << endl;
+
 
     if (read_img.fail()){
       cerr << "ERROR: Invalid file. Please check file name and try again." << endl;
@@ -26,7 +40,7 @@ int main(int argc, const char * argv[]) {
     if (read_img.is_open()){
       string line;
       while(getline(read_img, line)){
-        cout << line << endl;
+        // cout << line << endl;
       }
     }
 
