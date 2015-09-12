87e3969f497f280c5ddbfd4b23275929e7d3dc2f
Anna Karingal <anna.karingal@gmail.com>
2015-09-12 15:04:42 -0400

    Save input arguments to vars and open image file

--- p1.cpp
+++ p1.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
-#include <ctype.h>
+#include <fstream>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
 
-    if (argc != 3) {
-//        Error ahndling here
+    if (argc != 4) {
+      cerr << "ERROR: Invalid number of arguments." << endl;
+      exit(-1);
     }
+    // TODO: Check var types
+    string input_img_name = argv[1];
+    int threshold = argv[2];
+    string output_img_name = argv[3];
+
+    // Open image file
+    ifstream read_img; 
+    read_img.open(input_img_name.c_str());
+
+    if (read_img.fail()){
+      cerr << "ERROR: Invalid file. Please check file name and try again." << endl;
+      exit(-1);
+    }
+
+    if (read_img.is_open()){
+      string line;
+      while(getline(readf, line)){
+        cout << line << endl;
+      }
+    }
+
+
+    return 0;
     
 }
