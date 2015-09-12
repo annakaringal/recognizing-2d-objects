bcda6989da589910ba1d2458194d61f05664bd2f
Anna Karingal <anna.karingal@gmail.com>
2015-09-12 16:46:59 -0400

    Read image file and get headers

--- image_parser.cpp
+++ image_parser.cpp
@@ -17,3 +17,47 @@ bool is_valid_number(const char * str){
 
   return true;
 }
+
+PGMImg parseImage(ifstream &readf){
+  int width, height, max_gray;
+  string line, width_str, height_str;
+
+  // GET AND CHECK IMAGE HEADERS
+
+  // Check version
+  getline(readf, line);
+  if (line.compare("P5") != 0){
+    // TODO: Raise exception
+    cerr << "ERROR: Not a valid PGM File" << endl;
+  }
+
+  // Ignore comment
+  getline(readf, line);
+
+  // Get width & height
+  getline(readf, line);
+  istringstream ss(line);
+  getline(ss, width_str, ' ');
+  getline(ss, height_str);
+
+  // Get max gray
+  getline(readf, line);
+
+  try{
+    max_gray = stoi(line);
+    width = stoi(width_str);
+    height = stoi(height_str);
+  } catch (invalid_argument ia){
+    cerr << "ERROR: Invalid image headers (" << ia.what() << ")"<< endl;
+    exit(-1);
+  }
+
+  cout << "width: " << width << endl;
+  cout << "height: " << height << endl;
+  cout << "max_gray: " << max_gray << endl;
+
+  PGMImg img(width, height, max_gray);
+
+  return img;
+
+}
