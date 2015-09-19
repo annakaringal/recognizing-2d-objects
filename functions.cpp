41802f41b05a566773e772ecac6a2e412e23a097
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 15:10:13 -0400

    set binary_img pixel to 0 if < threshold; else set to 1

--- functions.cpp
+++ functions.cpp
@@ -15,11 +15,19 @@ bool isValidNumber(const char * str){
   return true;
 }
 
-int convertToBinary(Image* greyscale_img, Image* binary_img){
+int convertToBinary(Image* greyscale_img, Image* binary_img, int threshold){
   int rows = greyscale_img.getNRows();
   int cols = greyscale_img.getNCols();
   binary_img->setSize(rows, cols);
 
-
+  for (int i=0; i<rows; i++){
+    for (int j=0; j<cols; j++){
+      if (greyscale_img.getPixel(i, j) < threshold){
+        binary_img.setPixel(i, j, 0);
+      } else {
+        binary_img.setPixel(i, j, 1);
+      }
+    }
+  }
 
 }
