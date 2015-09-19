8f95b86ab35298a50ef3916235db9e118eef3f49
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 14:40:25 -0400

    remove debug line

--- functions.cpp
+++ functions.cpp
@@ -6,13 +6,10 @@
 using namespace std;
 
 bool is_valid_number(const char * str){
-  cout << "checking " << str << endl;
-
   for (int i=0; i < strlen(str); i++){
     if (!isdigit(str[i]) && str[i] != '.') {
       return false;
     }
   }
-
   return true;
 }
