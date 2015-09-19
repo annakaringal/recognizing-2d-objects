f834bab862c782b06c1f627de3bc17a3d4e35be3
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 15:28:20 -0400

    add comments for documentation. add error checking and support for negative numbers.
    - Add support for negative numbers
    - convertToBinary returns 0 if OK, -1 if something went wrong
    - convertToBinary checks if threshold is valid number

--- BinaryImage.cpp
+++ BinaryImage.cpp
@@ -7,27 +7,38 @@
 using namespace std;
 
 bool isValidNumber(const char * str){
+  // Return false if non-numeric character or if char is not . or -
   for (int i=0; i < strlen(str); i++){
-    if (!isdigit(str[i]) && str[i] != '.') {
+    if (!isdigit(str[i]) && str[i] != '.' && str[i] != '-') {
       return false;
     }
   }
   return true;
 }
 
-void convertToBinary(Image* greyscale_img, Image* binary_img, int threshold){
+int convertToBinary(Image* greyscale_img, Image* binary_img, int threshold){
+  // Check if threshold is valid
+  if (threshold > binary_img.getNColors() || threshold < 0) { return -1; }
+
+  // Set rows & cols of binary_img
   int rows = greyscale_img->getNRows();
   int cols = greyscale_img->getNCols();
   binary_img->setSize(rows, cols);
 
+  // Set colors of binary_img based on given threshold
   for (int i=0; i<rows; i++){
     for (int j=0; j<cols; j++){
       if (greyscale_img->getPixel(i, j) < threshold){
-        binary_img->setPixel(i, j, 0);
+        if (binary_img->setPixel(i, j, 0) < 0) {
+          return -1;
+        };
       } else {
-        binary_img->setPixel(i, j, 1);
+        if (binary_img->setPixel(i, j, 1) < 0){
+          return -1;
+        }
       }
     }
   }
 
+  return 0;
 }
