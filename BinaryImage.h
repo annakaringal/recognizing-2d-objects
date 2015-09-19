031c6f9213ba95c2ce8f2d6ea56fc4fa954fac41
Anna Karingal <anna.karingal@gmail.com>
2015-09-19 15:28:04 -0400

    add comments for documentation; convertToBinary returns int

--- BinaryImage.h
+++ BinaryImage.h
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+// Returns true if char * is a decimal number
 bool isValidNumber(const char* str);
 
-void convertToBinary(Image* greyscale_img, Image* binary_img, int threshold);
+// Converts greyscale_image to binary based on passed threshold argument
+// Saves result to binary_imag
+// Preconditions:   - greyscale_img is a greyscale pgm image file
+//                  - binary_img is a pointer to an empty initailized Image
+//                  - threshold is an int between 0 to the greyscale_img.getColors()
+// Returns 0 if OK, else returns -1 if something went wrong
+int convertToBinary(Image* greyscale_img, Image* binary_img, int threshold);
