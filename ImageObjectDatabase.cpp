ecab6374ce43f0ffc5c7079a27c895c39afd0dfc
Anna Karingal <anna.karingal@gmail.com>
2015-09-27 15:07:32 -0400

    add missing type declarations

--- ImageObjectDatabase.cpp
+++ ImageObjectDatabase.cpp
@@ -2,7 +2,7 @@
 
 int ImageObjectDatabase::writeDatabase(const char* fname){
   for (int i = 1; i <= num_objects; i++){
-    obj = getObject(i);
+    Object* obj = getObject(i);
     cout << "label: " << obj->getLabel() << " | ";
     cout << "area: " << obj->getArea() << " | ";
     cout << "p min: " << obj->getOrientation().first << " | ";
@@ -19,7 +19,7 @@ void ImageObjectDatabase::generateObjects(){
   for (int i = 0; i < num_objects; i++){
     // Create new empty object and add to object array
     int label = i+1;
-    Object obj(label);
+    Object* obj(label);
     objects[i] = &obj;
   }
 
@@ -32,9 +32,9 @@ void ImageObjectDatabase::calculateObjectProperties(){
   // update properties of obj labelled at pixel
   for (int i=0; i<img->getNRows(); i++){
     for (int j=0; j<img->getNCols(); j++){
-      label = img->getPixel(i,j);
+      int label = img->getPixel(i,j);
       if (label > 0){
-        obj = getObject(label);
+        Object* obj = getObject(label);
         obj->updateXYProperties(i,j);
       }
     }
@@ -42,7 +42,7 @@ void ImageObjectDatabase::calculateObjectProperties(){
 
   // Calculate center, orientation and moment for each obj
   for (int i=1; i<=num_objects; i++){
-    obj = getObject(i);
+    Object* obj = getObject(i);
     obj->calculateCenter();
     obj->calculateOrientation();
   }
