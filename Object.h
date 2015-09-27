ac2f98f5667205a1815431eaef8ec4faa9edf622
Anna Karingal <anna.karingal@gmail.com>
2015-09-27 14:25:59 -0400

    add methods to get and calculate center & are

--- Object.h
+++ Object.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 #include "pgm/Image.h"
 
@@ -11,27 +12,30 @@ using namespace std;
 class Object{
 
 public:
-  Object(int l): label(l){};
+  Object(int l, int area=0, int xS=0, int yS=0, int xxS=0, int yyS=0, int xy=0)
+        : label(l), area(a), xSum(xS), ySum(yS), xSqSum(xxS), ySqSum(yyS), xySum(xy){};
 
-  int calculateProperties(){
-    calcMoment();
-    calcOrientation();
-    calcCenter();
-    calcArea();
-  }
+  int getArea() const { return area; };
+
+  int getxSum() const { return xSum; };
+
+  int getySum() const { return ySum; };
+
+  pair<float,float> getCenter() const {
+    return pair<float, float> (centerRow, centerCol);
+  };
+
+  pair<float,float> calculateCenter();
 
 private:
   int label;
-  int centerRow, centerCol;
+  float centerRow, centerCol;
   float moment;
   float orientation;
-  float area;
-
-  int calcMoment();
-  int calcOrientation();
-  int calcCenter();
-  int calcArea();
-
+  int area;
+  int xSum, ySum;
+  int xSqSum, ySqSum; 
+  int xy;
 };
 
 #endif
