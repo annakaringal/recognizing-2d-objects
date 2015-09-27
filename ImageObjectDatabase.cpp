29063476e227e66b860720223a2e068f59f5ed75
Anna Karingal <anna.karingal@gmail.com>
2015-09-27 14:25:28 -0400

    generate objects form mimage and calculate center and orientation

--- ImageObjectDatabase.cpp
+++ ImageObjectDatabase.cpp
@@ -4,6 +4,37 @@ int ImageObjectDatabase::writeDatabase(const char* fname){
 
 }
 
-void ImageObjectDatabase::generateObjects(labeled_img){
+void ImageObjectDatabase::generateObjects(){
+
+  for (int i = 0; i < num_objects; i++){
+    // Create new empty object and add to object array
+    int label = i+1;
+    Object obj(label);
+    objects[i] = &obj;
+  }
+
+  calculateObjectProperties();
+}
+
+void ImageObjectDatabase::calculateObjectProperties(){
+
+  // Scan image pixel by pixel
+  // update properties of obj labelled at pixel
+  for (int i=0; i<img->getNRows(); i++){
+    for (int j=0; j<img->getNCols(); j++){
+      label = img->getPixel(i,j);
+      if (label > 0){
+        obj = getObject(label);
+        obj->updateXYProperties(i,j);
+      }
+    }
+  }
+
+  // Calculate center, orientation and moment for each obj
+  for (int i=1; i<=num_objects; i++){
+    obj = getObject(label);
+    obj->calculateCenter();
+    obj->calculateOrientation();
+  }
 
 }
