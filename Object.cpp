fb11eee8e946a6488ff15fe56777133d40d35a09
Anna Karingal <anna.karingal@gmail.com>
2015-09-27 14:22:40 -0400

    implement methods to update x & y properties given pixel position and calculate center of object

--- Object.cpp
+++ Object.cpp
@@ -1,1 +1,23 @@
 #include "Object.h"
+
+void Object::setArea(int a){ area = a; }
+
+void Object::setCenter(int r, int c){ 
+  centerRow = r; 
+  centerCol = c;
+ };
+
+void Object::updateXYProperties(int x, int y){
+  area++;
+  xSum += x; 
+  ySum += y;
+  xSqSum += (x*x);
+  ySqSum += (y*y);
+  xySum += (x*y);
+};
+
+pair<float,float> Object::calculateCenter(){
+  centerRow = xSum / area;
+  centerCol = yXum / area;
+  return pair<float,float> (centerRow, centerCol);
+}
