a16fe7b96611c869a66602eeb9bc227fc836192e
Anna Karingal <anna.karingal@gmail.com>
2015-09-27 20:55:44 -0400

    comment valid radians function

--- Validation.h
+++ Validation.h
@@ -8,9 +8,12 @@
 
 using namespace std;
 
-// Convert angle in radians to value between 0 and 2 PI
+// Convert angle in radians to value between 0 and 2*PI
+// Precondition: angle is in Radians
+// Returns the equivalent value of original angle within 0 and 2*PI
 float validRadians(float angle);
 
+// Check if typename S can be cast to typename T
 template<typename T, typename S> bool isValidType(const S str){
   istringstream ss(str);
   T f;
