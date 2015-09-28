2c941d63b4c8dc7b113f7514a29cb49f576298a3
Anna Karingal <anna.karingal@gmail.com>
2015-09-27 21:01:45 -0400

    add comments to explain class member functions

--- ImageObjectDatabase.h
+++ ImageObjectDatabase.h
@@ -18,30 +18,37 @@ using namespace std;
 class ImageObjectDatabase{
 
 public:
-  // create image object database from image
+  // Create image object database from image
   ImageObjectDatabase(Image* labeled_img) : img (labeled_img) {
     num_objects = labeled_img->getColors();
     generateObjects();
   };
 
+  // Create empty image object db
   ImageObjectDatabase() { 
     num_objects = 0;
   };
 
+  // Make sure all objects are deallocated
   ~ImageObjectDatabase(){
     for (int i=0; i<objects.size(); i++){
       delete objects[i];
     }
   }
 
+  // Returns number of objects in database
   int getNumObjects() const { return num_objects; };
 
+  // Write database to file
   int writeDatabase(const char* fname);
 
+  // Returns pointer to object with label
   Object* getObject(int label) const { return objects[label-1]; };
 
+  // Returns label of object match if obj has a close match within the database
   int hasMatch(Object* obj);
 
+  // Add a new object to the database
   void addObject(Object* obj);
 
 private:
@@ -49,11 +56,16 @@ private:
   int num_objects;
   vector<Object*> objects;
 
+  // Create new objects from img with all object attributes and properties
+  // Precondition: img exists
   void generateObjects();
 
+  // Calculate properties of all objects in image
+  // Precondition: objects is a vector containing only and all objects in img
   void calculateObjectProperties();
 };
 
+// Read existing database from file and populate iodb with data
 int readDatabase(ImageObjectDatabase* iodb, const char* fname);
 
 #endif
