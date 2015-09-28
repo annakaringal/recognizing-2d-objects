ae4bc54694f2276d59c24457733ed9a20c408fbf
Anna Karingal <anna.karingal@gmail.com>
2015-09-27 21:13:37 -0400

    add comments to clarify member functions

--- Object.h
+++ Object.h
@@ -18,8 +18,26 @@ public:
   Object(int l, int a=0, int xS=0, int yS=0, int xxS=0, int yyS=0, int xy=0)
         : label(l), area(a), xSum(xS), ySum(yS), xSqSum(xxS), ySqSum(yyS), xySum(xy){};
 
+  // Given x and y coordinates of pixel, update the xSum, ySum, 
+  // xSqSum, xSqSum and xySum of the object so propertie can be calculated
+  // in one scan.
   void updateXYProperties(int x, int y);
 
+  // Calculates center coordinates and sets member variables
+  // Precondition: full scan has been preformed to update xSum, ySum of object
+  pair<float,float> calculateCenter();
+
+  // Calculates center coordinates and sets member variables
+  // Precondition: full scan has been preformed to update xSum, ySum of object
+  void calculateOrientation();
+
+  // Calculates orientation properties (min/max angle, min/max momentum, orientation line)
+  // and sets member variables
+  // Precondition: full scan has been preformed to update/calculate xSqSum, ySqSum,
+  // xySum and center coordinates.
+  void calculateOrientationLine();
+
+  // Get object attributes
   int getLabel() const { return label; };
 
   int getArea() const { return area; };
@@ -34,14 +52,7 @@ public:
     return pair<float, float> (centerRow, centerCol);
   };
 
-  pair<float,float> calculateCenter();
-
-  void calculateOrientation();
-
-  void calculateOrientationLine();
-
-  float calculateMoment(float a, float b, float c, float angle);
-
+  // Set object attributes
   void setCenter(float r, float c);
 
   void setArea(int a);
@@ -63,6 +74,8 @@ private:
   int xSum, ySum;
   int xSqSum, ySqSum; 
   int xySum;
+
+  float calculateMoment(float a, float b, float c, float angle);
 };
 
 
